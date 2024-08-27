f963a27b57b60e5c8d331568bf94a216c7a0673e
M_Waqas_Anwar <m.waqas.anwar.121@gmail.com>
2024-08-28 00:51:29 +0500

    new changes

--- first.py
+++ first.py
@@ -1,22 +1,26 @@
+#friuts=['apple','banana','orange']
+#x,y,z=friuts
+#print(x)
+#print(y)
+#print(z)
+#x1="python "
+#x2="is "
+#x3="awesome"
+#print(x1 + x2 + x3)
+#x6 = "Me"
+#def myfunc():
+#  print("You see " + x6)
+#myfunc()
 
+name=input("Enter Your Name : ").strip().title()
 
-friuts=['apple','banana','orange']
-x,y,z=friuts
-print(x)
-print(y)
-print(z)
+#remove all sapce enter by user
+#name=name.strip()
+#first letter capital 
+#name=name.capitalize()
+#all first letter capital 
+#name=name.title()
+#remove all sapce enter by user and all first letter capital
+#name=name.strip().title()
 
-x1="python "
-x2="is "
-x3="awesome"
-print(x1 + x2 + x3)
-
-x6 = "Me"
-
-def myfunc():
-  print("You see " + x6)
-
-myfunc()
-
-name=input("Enter Your Name")
 print(f"Hello {name}")
